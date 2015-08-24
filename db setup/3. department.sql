e88672f18b76bd46ac9e576126ab1c5db33e2f78
Zetsukaze <indolent.guzzy@gmail.com>
2015-08-25 05:09:35 +0800

    Creation of maintain department menu

---
+++ db setup/3. department.sql
@@ -0,0 +1,22 @@
+drop table if exists tbl_departments;
+
+create table tbl_departments (
+	id varchar (32) not null,
+  dept_name varchar (8) not null,
+  dept_desc varchar (256),
+  version integer,
+  primary key (id)
+);
+
+drop table if exists tbl_departments_aud;
+
+create table tbl_departments_aud (
+  id varchar (32) not null,
+  rev int (11) not null,
+  rev_type tinyint (4) default null,
+  dept_name varchar (8) not null,
+  dept_desc varchar (256),
+  version integer,
+  primary key (id, rev),
+  foreign key (rev) references tbl_audit_rev_info (id)
+);
