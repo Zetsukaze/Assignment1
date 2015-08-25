bf4be51eb041bb6526bc497d2c7854912b851d7d
Zetsukaze <indolent.guzzy@gmail.com>
2015-08-25 10:58:29 +0800

    Update data fields due to functional requirements

--- db setup/3. department.sql
+++ db setup/3. department.sql
@@ -2,8 +2,8 @@ drop table if exists tbl_departments;
 
 create table tbl_departments (
 	id varchar (32) not null,
-  dept_name varchar (8) not null,
-  dept_desc varchar (256),
+  dept_name varchar (100) not null,
+  dept_desc varchar (200),
   version integer,
   primary key (id)
 );
@@ -14,8 +14,8 @@ create table tbl_departments_aud (
   id varchar (32) not null,
   rev int (11) not null,
   rev_type tinyint (4) default null,
-  dept_name varchar (8) not null,
-  dept_desc varchar (256),
+  dept_name varchar (100) not null,
+  dept_desc varchar (200),
   version integer,
   primary key (id, rev),
   foreign key (rev) references tbl_audit_rev_info (id)
